d794694838db71d57f26764a2c980f94e90c5afb
Ariiadnee-06 <ariadne.castaneda844@pascualbravo.edu.co>
2025-03-16 22:02:10 -0500

    Agregamos los servicios al core

---
+++ HP3_Evaluacion1.Core/Servicios/IFestivoServicio.cs
@@ -0,0 +1,18 @@
+﻿using HP3_Evaluacion1.Dominio.Entidades;
+namespace HP3_Evaluacion1.Core.Servicios
+{
+    public interface IFestivoServicio
+    {
+        Task<IEnumerable<Festivo>> ObtenerTodos();
+
+        Task<Festivo> Obtener(int id);
+
+        Task<Festivo> Agregar(Festivo festivo);
+
+        Task<Festivo> Modificar(Festivo festivo);
+
+        Task<bool> Eliminar(int id);
+
+        Task<IEnumerable<Festivo>> Buscar(int idTipo, string dato);
+    }
+}
---
+++ HP3_Evaluacion1.Core/Servicios/ITipoServicio.cs
@@ -0,0 +1,19 @@
+﻿using HP3_Evaluacion1.Dominio.Entidades;
+
+namespace Festivos.Core.Servicios
+{
+    public interface ITipoServicio
+    {
+        Task<IEnumerable<Tipo>> ObtenerTodos();
+
+        Task<Tipo> Obtener(int id);
+
+        Task<Tipo> Agregar(Tipo tipo);
+
+        Task<Tipo> Modificar(Tipo tipo);
+
+        Task<bool> Eliminar(int id);
+
+        Task<IEnumerable<Tipo>> Buscar(string dato);
+    }
+}
