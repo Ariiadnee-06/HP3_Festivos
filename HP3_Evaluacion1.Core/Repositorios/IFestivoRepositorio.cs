3cb4bf244001bb8f9782fb94c09d76e48a2f3329
Ariiadnee-06 <ariadne.castaneda844@pascualbravo.edu.co>
2025-03-16 21:57:50 -0500

    Cambios del Core

---
+++ HP3_Evaluacion1.Core/Repositorios/IFestivoRepositorio.cs
@@ -0,0 +1,24 @@
+﻿using HP3_Evaluacion1.Dominio.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HP3_Evaluacion1.Core.Repositorios
+{
+    public interface IFestivoRepositorio
+    {
+        Task<IEnumerable<Festivo>> ObtenerTodos();
+
+        Task<Festivo> Obtener(int id);
+
+        Task<Festivo> Agregar(Festivo festivo);
+
+        Task<Festivo> Modificar(Festivo festivo);
+
+        Task<bool> Eliminar(int id);
+
+        Task<IEnumerable<Festivo>> Buscar(int idTipo, string dato);
+    }
+}
---
+++ HP3_Evaluacion1.Core/Repositorios/ITipoRepositorio.cs
@@ -0,0 +1,17 @@
+﻿using HP3_Evaluacion1.Dominio.Entidades;
+
+namespace Festivos.Core.Repositorios
+{
+    public interface ITipoRepositorio
+    {
+        Task<IEnumerable<Tipo>> ObtenerTodos();
+
+        Task<Tipo> Obtener(int id);
+
+        Task<Tipo> Agregar(Tipo tipo);
+
+        Task<Tipo> Modificar(Tipo tipo);
+
+        Task<bool> Eliminar(int id);
+    }
+}
