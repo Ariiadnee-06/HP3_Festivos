e2cdd1682aef9035b33741e2e0bc58f753380921
Ariiadnee-06 <ariadne.castaneda844@pascualbravo.edu.co>
2025-03-27 09:37:28 -0500

    Se agrego .Aplicacion

---
+++ HP3_Evaluacion1.Aplicacion/Servicios/FestivoServicio.cs
@@ -0,0 +1,46 @@
+﻿using HP3_Evaluacion1.Core.Repositorios;
+using HP3_Evaluacion1.Core.Servicios;
+using HP3_Evaluacion1.Dominio.Entidades;
+
+namespace HP3_Evaluacion1.Aplicacion.Servicios
+{
+    public class FestivoServicio : IFestivoServicio
+    {
+        private readonly IFestivoRepositorio repositorio;
+
+        public FestivoServicio(IFestivoRepositorio repositorio)
+        {
+            this.repositorio = repositorio;
+        }
+
+        public async Task<Festivo> Agregar(Festivo festivo)
+        {
+            return await repositorio.Agregar(festivo);
+        }
+
+        public async Task<IEnumerable<Festivo>> Buscar(int tipo, string dato)
+        {
+            return await repositorio.Buscar(tipo, dato);
+        }
+
+        public async Task<bool> Eliminar(int id)
+        {
+            return await repositorio.Eliminar(id);
+        }
+
+        public async Task<Festivo> Modificar(Festivo festivo)
+        {
+            return await repositorio.Modificar(festivo);
+        }
+
+        public async Task<Festivo> Obtener(int id)
+        {
+            return await repositorio.Obtener(id);
+        }
+
+        public async Task<IEnumerable<Festivo>> ObtenerTodos()
+        {
+            return await repositorio.ObtenerTodos();
+        }
+    }
+}
---
+++ HP3_Evaluacion1.Aplicacion/Servicios/TipoServicio.cs
@@ -0,0 +1,48 @@
+﻿using Festivos.Core.Repositorios;
+using Festivos.Core.Servicios;
+using HP3_Evaluacion1.Core.Repositorios;
+using HP3_Evaluacion1.Core.Servicios;
+using HP3_Evaluacion1.Dominio.Entidades;
+
+namespace HP3_Evaluacion1.Aplicacion.Servicios
+{
+    public class TipoServicio : ITipoServicio
+    {
+        private readonly ITipoRepositorio repositorio;
+
+        public TipoServicio(ITipoRepositorio repositorio)
+        {
+            this.repositorio = repositorio;
+        }
+
+        public async Task<Tipo> Agregar(Tipo tipo)
+        {
+            return await repositorio.Agregar(tipo);
+        }
+
+        public async Task<IEnumerable<Tipo>> Buscar(string dato)
+        {
+            return await repositorio.Buscar(dato);
+        }
+
+        public async Task<bool> Eliminar(int id)
+        {
+            return await repositorio.Eliminar(id);
+        }
+
+        public async Task<Tipo> Modificar(Tipo tipo)
+        {
+            return await repositorio.Modificar(tipo);
+        }
+
+        public async Task<Tipo> Obtener(int id)
+        {
+            return await repositorio.Obtener(id);
+        }
+
+        public async Task<IEnumerable<Tipo>> ObtenerTodos()
+        {
+            return await repositorio.ObtenerTodos();
+        }
+    }
+}
