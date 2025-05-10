4c87970e4fddb7c6bd1687539927c3f9200c6438
Ariiadnee-06 <ariadne.castaneda844@pascualbravo.edu.co>
2025-05-10 08:58:26 -0500

    otra parte

--- HP3_Evaluacion1.Presentacion/WeatherForecast.cs
+++ HP3_Evaluacion1.Presentacion/WeatherForecast.cs
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 ﻿namespace HP3_Evaluacion1.Presentacion
 {
     public class WeatherForecast
@@ -11,3 +12,18 @@
         public string? Summary { get; set; }
     }
 }
+=======
+﻿namespace HP3_Evaluacion1.Presentacion
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
+>>>>>>> 6331e77 (Agregar archivos de proyecto.)
